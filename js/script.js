169ba6466cc2763d29c43b67c8bca12300e4448e
Parker Eakin <parker.eakin@gmail.com>
2018-08-27 16:13:01 -0600

    Requirements Outlined
    Requirements Outlined in comments in the script.js file. Also added the Jquery.js file, and linked both js files to the index.html file.

---
+++ js/script.js
@@ -0,0 +1,73 @@
+/*
+	Techdegree Project: Unit 3 - Interactive Form for Registration
+*/
+
+
+// Use Jquery
+
+
+// Automatically focus on first field after page load
+
+
+// Job Role - If user selects "other" under Job Role, a new text field appears
+
+
+// Color drop down menu is hidden until a T-shirt design is selected.
+
+
+// Make sure User can't select two activites occuring at the same time.
+
+
+// Calculate and display total cost of the selected activities, below the list of activities.
+
+
+// Credit Card payment option is selected by default
+
+
+// Payment option in the select menu matches the payment option displayed on the page
+
+
+// When a user chooses a payment option, the chosen payment section is revealed and the other payment sections are hidden
+
+
+
+//Form cannot be submitted (the page does not refresh when the submit button is clicked) until the following requirements have been met:
+
+	// Name field isn’t blank.
+
+	// Email field contains validly formatted e-mail address: (doesn’t have to check that it's a real e-mail address, just that it's formatted like one: dave@teamtreehouse.com, for example).
+
+	// At least one checkbox under "Register for Activities" section must be selected.
+
+	// If "Credit Card" is the selected payment option, the three fields accept only numbers: a 13 to 16-digit credit card number, a 5-digit zip code, and 3-number CVV value.
+
+
+
+// On submission, the form provides an error indication or message for each field that requires validation:
+
+	// Name field
+
+	// Email field
+
+	// “Register for Activities” checkboxes
+
+	// Credit Card number, Zip code, and CVV, only if the credit card payment method is selected.
+
+	// Form provides at least one error message in real time, before the form is submitted. For example, the error message appears near the email field when the user begins to type, and disappears as soon as the user has entered a complete and correctly formatted email address.
+
+	// Form provides at least one error message that changes depending on the error. For example, the email field displays a different error message when the email field is empty than it does when the email address is formatted incorrectly. *This is accomplished without the use of HTML5's built-in field validation.
+
+
+
+// When JavaScript is disabled, all form fields and payment information is displayed, including the "Other" field under the "Job Role" section.
+
+
+
+
+
+
+
+
+
+
+
