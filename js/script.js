51d5c2d28a12c0afd7de8eefb3dd6b02e94665b5
Parker Eakin <parker.eakin@gmail.com>
2018-08-28 21:56:03 -0600

    Prevent Submission and Alert If Errors Occur

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@
 	Techdegree Project: Unit 3 - Interactive Form for Registration
 */
 
+// Global Variables
+	let activities = $('.activities input');
+	let numChecked = $('.activities :checked').length;
+	let ccNum = $('#cc-num').val();
+	let checkCCNum = checkIntegers(ccNum);
+	let zipNum = $('#zip').val();
+	let checkZipNum = checkIntegers(zipNum);
+	let cvvNum = $('#cvv').val();
+	let checkCvvNum = checkIntegers(cvvNum);
+
 
 // Automatically focus on first field after page load
 	$('#name').focus();
@@ -80,7 +90,7 @@
 
 // Make sure User can't select two activites occuring at the same time.
 	// Splits the strings to get time. Creates an array, then adds and removes times of clicked or uncliked inputs. If input is clicked, and the time already exists in the array, an alert pops up and the input is unchecked.
-	var activities = $('.activities input');
+
 	var timeArray = [];
 	var priceArray = [0];
 
@@ -134,7 +144,6 @@
 		}
 
 		var totalPrice = priceArray.reduce(getSum);
-		console.log(totalPrice);
 
 		displayPrice.textContent = "Your Total: $" + totalPrice;
 
@@ -179,13 +188,133 @@
 
 //Form cannot be submitted (the page does not refresh when the submit button is clicked) until the following requirements have been met:
 
-	// Name field isn’t blank.
+	function checkIntegers(num) {
+		let isInteger = Number.isInteger(parseInt(num));
+		return isInteger;
+	}
 
+	$('button').on('click', function(e) {
+		let name = $('#name').val();
+		let email = $('#mail').val();
+		let dotCom = email.indexOf('.');
+		numChecked = $('.activities :checked').length;
+		let paymentOption = paymentMethod.val();
+
+		if (name === "") {
+			e.preventDefault();
+			alert('You forgot to enter your name!');
+			$('#name').focus();
+			$('input#name:focus').css('border-color', 'red');
+			$('#name').prev().css('color', 'red');
+
+		} else if (dotCom === -1 ) {
+			e.preventDefault();
+			alert('Your email address in incomplete.');
+			$('#mail').focus();
+			$('input#mail:focus').css('border-color', 'red');
+			$('#mail').prev().css('color', 'red');
+
+		} else if (numChecked === 0 ) {
+			e.preventDefault();
+			alert('You must select an activity');
+			$('.activities').css('background-color', 'rgba(255, 0, 0, 0.3)');
+			$([document.documentElement, document.body]).animate({
+		        scrollTop: $(".activities").offset().top
+		    }, 600);
+
+		} else if (paymentOption === "credit card") {
+			ccNum = $('#cc-num').val();
+			checkCCNum = checkIntegers(ccNum);
+			zipNum = $('#zip').val();
+			checkZipNum = checkIntegers(zipNum);
+			cvvNum = $('#cvv').val();
+			checkCvvNum = checkIntegers(cvvNum);
+
+			if (checkCCNum === false || ccNum.length < 13 || ccNum.length > 16) {
+				e.preventDefault();
+				alert('The Credit Card number is invalid.');
+				$('#cc-num').focus();
+				$('input#cc-num:focus').css('border-color', 'red');
+				$('#cc-num').prev().css('color', 'red');
+
+			} else if (checkZipNum === false || zipNum.length !== 5) {
+				e.preventDefault();
+				alert('The Zip Code must be 5 digits.');
+				$('#zip').focus();
+				$('input#zip:focus').css('border-color', 'red');
+				$('#zip').prev().css('color', 'red');
+
+			} else if (checkCvvNum === false || cvvNum.length !== 3) {
+				e.preventDefault();
+				alert('The CVV value must be 3 numbers.');
+				$('#cvv').focus();
+				$('input#cvv:focus').css('border-color', 'red');
+				$('#cvv').prev().css('color', 'red');
+			}
+
+		}
+	});
+
+
+	// Name field isn’t blank.
+	$('#name').on('change', function(e) {
+		let name = $('#name').val();
+		if (name !== "") {
+			$('input#name').css('border-color', 'transparent');
+			$('input#name:focus').css('border-color', '#5e97b0');
+			$('#name').prev().css('color', '#000');
+		}
+	});
+
+	
 	// Email field contains validly formatted e-mail address: (doesn’t have to check that it's a real e-mail address, just that it's formatted like one: dave@teamtreehouse.com, for example).
+	$('#mail').on('change', function(e) {
+		let email = $('#mail').val();
+		if (email !== "") {
+			$('input#mail').css('border-color', 'transparent');
+			$('input#mail:focus').css('border-color', '#5e97b0');
+			$('#mail').prev().css('color', '#000');
+		}
+	});
+
 
 	// At least one checkbox under "Register for Activities" section must be selected.
+	$(activities).on('change', function(e) {
+		numChecked = $('.activities :checked').length;
+		if (numChecked !== 0) {
+			$('.activities').css('background-color', 'transparent');
+		}
+	});
 
 	// If "Credit Card" is the selected payment option, the three fields accept only numbers: a 13 to 16-digit credit card number, a 5-digit zip code, and 3-number CVV value.
+	$('#cc-num').on('change', function(e) {
+		ccNum = $('#cc-num').val();
+		checkCCNum = checkIntegers(ccNum);
+		if (checkCCNum === true  && ccNum.length >= 13 && ccNum.length <= 16) {
+			$('input#cc-num').css('border-color', 'transparent');
+			$('input#cc-num:focus').css('border-color', '#5e97b0');
+			$('#cc-num').prev().css('color', '#000');
+		}
+	});
+	$('#zip').on('change', function(e) {
+		zipNum = $('#zip').val();
+		checkZipNum = checkIntegers(zipNum);
+		if (checkZipNum === true && zipNum.length === 5) {
+			$('input#zip').css('border-color', 'transparent');
+			$('input#zip:focus').css('border-color', '#5e97b0');
+			$('#zip').prev().css('color', '#000');
+		}
+	});
+	$('#cvv').on('change', function(e) {
+		cvvNum = $('#cvv').val();
+		checkCvvNum = checkIntegers(cvvNum);
+		if (checkCvvNum === true && cvvNum.length === 3) {
+			$('input#cvv').css('border-color', 'transparent');
+			$('input#cvv:focus').css('border-color', '#5e97b0');
+			$('#cvv').prev().css('color', '#000');
+		}
+	});
+
 
 
 
@@ -199,9 +328,6 @@
 
 	// Credit Card number, Zip code, and CVV, only if the credit card payment method is selected.
 
-	// Form provides at least one error message in real time, before the form is submitted. For example, the error message appears near the email field when the user begins to type, and disappears as soon as the user has entered a complete and correctly formatted email address.
-
-	// Form provides at least one error message that changes depending on the error. For example, the email field displays a different error message when the email field is empty than it does when the email address is formatted incorrectly. *This is accomplished without the use of HTML5's built-in field validation.
 
 
 
